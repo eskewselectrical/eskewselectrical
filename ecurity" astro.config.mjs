fb08ec32a6b442e6fc02c5c04cf35dc8588a2fb5
ehester11 <211704122+ehester11@users.noreply.github.com>
2026-07-16 13:54:58 -0500

    updated verbage

---
+++ ecurity" astro.config.mjs
@@ -0,0 +1,3 @@
+[33m69b1a17[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m fix contact form
+[33mddae31d[m Merge pull request #1 from eskewselectrical/vercel/install-vercel-web-analytics-wj2caz
+[33m2dea87b[m Install Vercel Web Analytics
